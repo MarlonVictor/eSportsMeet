52bf9c857937421dba1285b1dcb61324118ffc15
MarlonVictor <cmarlonvictor11@gmail.com>
2022-09-15 20:53:24 -0300

    Starting prisma configuration with sqlite and creating first tables

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,24 @@ import express from 'express';
 
 const app = express()
 
-app.get('/user', (req, res) => {
-  return res.json([
-    {
-      name: 'Marlon Victor',
-      age: 20
-    }
-  ])
+app.get('/games', (req, res) => {
+  return res.json([])
+})
+
+app.get('/games/:id/ads', (req, res) => {
+  const gameId = req.params.id;
+
+  return res.json([ gameId ])
+})
+
+app.get('/ads/:id/discord', (req, res) => {
+  const adId = req.params.id;
+
+  return res.json([ adId ])
+})
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
 })
 
 app.listen(3333)
